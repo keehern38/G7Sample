12b6c8aa9030e5252ebfc022915811cde39ad3e1
Tarc <Tarc@B91>
2018-11-01 10:23:26 +0800

    completed ###

---
+++ src/g7sample/G7Sample.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g7sample;
+
+/**
+ *
+ * @author Tarc
+ */
+public class G7Sample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World 1");
+    }
+    
+}
